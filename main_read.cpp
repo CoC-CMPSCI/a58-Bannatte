bbf3c478b81e937c4b9510be498c632077edd2e6
Bennett <32626837+Bannatte@users.noreply.github.com>
2026-04-12 22:37:47 -0700

    Refactor main_read.cpp to streamline file reading

--- main_read.cpp
+++ main_read.cpp
@@ -1,27 +1,36 @@
 #include <iostream>
 #include <fstream>
-#include <iomanip>
-#include <string>
 using namespace std;
-int main()
-{
+
+int main() {
+    ifstream inFile("students.txt");
+
+    if (!inFile) {
+        cout << "Error opening file for reading." << endl;
+        exit(0);
+    }
+
     int M;
-    double score1, score2, sum, avg;
-    string sname;
-    ifstream ifs;
-    ifs.open("students.txt");
-    if(!ifs){
-        cerr << "Failed to open file." << endl;
-        return 1;
+    inFile >> M;
+
+    cout << "Total " << M << " students" << endl;
+
+    string name;
+    int score1, score2;
+
+    for (int i = 0; i < M; i++) {
+        inFile >> name >> score1 >> score2;
+
+        int sum = score1 + score2;
+        double avg = sum / 2.0;
+
+        cout << "Student Name: " << name
+             << " score1: " << score1
+             << " score2: " << score2
+             << " Sum: " << sum
+             << " Avg: " << avg << endl;
     }
-    ifs >> M;
-    for (int i = 0; i < M; ++i) 
-    {
-        ifs >> sname >> score1 >> score2;
-        sum = score1 + score2;
-        avg = sum / 2.0;
-        cout << "Student Name: " << sname << " score1: " << score1 << " score2: " << score2 << " Sum: " << sum << " Avg: " << avg << endl;
-    }
-    ifs.close();
+
+    inFile.close();
     return 0;
 }
